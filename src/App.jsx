f89b81e2df94a0e3f347132dfd189cade52caa8a
lysychas <m80silver@gmail.com>
2021-08-06 16:10:11 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-// import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
-// import Register from "./pages/register/Register";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 // TODO: wrap buttons in IconButton
 // TODO: switch to SASS OR styled-components OR Tailwind
 // TODO: add React-Router
 
 function App() {
-  return <Profile />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
