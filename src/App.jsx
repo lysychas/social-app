514f89f662357eb6d50111ca66040ce6b1fa06af
lysychas <m80silver@gmail.com>
2021-08-06 13:33:24 +0300

    mapping and styling

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+// import Register from "./pages/register/Register";
+
+// TODO: wrap buttons in IconButton
+// TODO: switch to SASS OR styled-components OR Tailwind
+// TODO: add React-Router
+
+function App() {
+  return <Profile />;
+}
+
+export default App;
